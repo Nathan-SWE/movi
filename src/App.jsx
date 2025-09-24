efd210509a5b69397db86f935eb068eda2c1c2a4
Nathan Lima <nathanls.swe@gmail.com>
2025-09-23 21:43:36 -0300

    feat: implement onAuthStateChanged listener to sync state

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./services/firebase";
+import { useAuthStore } from "./stores/useAuthSore";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const setUser = useAuthStore((state) => state.setUser);
+  const setLoading = useAuthStore((state) => state.setLoading);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, [setUser, setLoading]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img
+        src="https://i.pinimg.com/originals/ea/6f/95/ea6f95a379addc1c241ed91391974a86.gif"
+        alt="pochita"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
