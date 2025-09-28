a7aa5a7d431cb09387877258c8f0e94d12a18ecb
Nathan Lima <nathanls.swe@gmail.com>
2025-09-28 17:22:23 -0300

    refactor: import MantineUi, style files, and implement Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+
+import { theme } from "./theme.js";
+import { Router } from "./routes";
+import "./global.css";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <MantineProvider theme={theme}>
+        <Router />
+      </MantineProvider>
     </QueryClientProvider>
   </StrictMode>
 );
