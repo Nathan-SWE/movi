ebe0e4c4c08de96550760116926a33f1c4954763
Nathan Lima <nathanls.swe@gmail.com>
2025-10-06 21:59:59 -0300

    refactor: keep only the authPage centralized

--- src/pages/Auth/AuthPage.jsx
+++ src/pages/Auth/AuthPage.jsx
@@ -5,6 +5,7 @@ import GoogleLogo from "../../assets/icon-google.svg";
 import {
   Container,
   Stack,
+  Flex,
   Title,
   Text,
   Button,
@@ -15,49 +16,49 @@ import {
 
 export default function AuthPage() {
   return (
-    <Container size="xs">
-      <Stack gap="xl">
-        <Container>
-          <ImageSkeleton
-            src="/movi.svg"
-            alt="Movi Logo"
-            w={140}
-            h={140}
-            mx="auto"
-          />
-          <Title order={1} ta="center">
-            MOVI
-          </Title>
-          <Text c="dimmed" ta="center" my="xs">
-            Sign up now and start your cinematic journey!
-          </Text>
-        </Container>
-
-        <Button component={Link} to="/auth/signup" size="lg" fullWidth>
-          Sign Up
-        </Button>
-
-        <Text ta="center" fz="sm">
-          I already have an account?
-          <Anchor component={Link} to="auth/login">
-            <Text component="span" weight={700} px="xs">
-              Login
+    <Flex h="100vh" align="center" justify="center">
+      <Container size="xs">
+        <Stack gap="xl">
+          <Container>
+            <ImageSkeleton
+              src="/movi.svg"
+              alt="Movi Logo"
+              w={140}
+              h={140}
+              mx="auto"
+            />
+            <Title order={1} ta="center">
+              MOVI
+            </Title>
+            <Text c="dimmed" ta="center" my="xs">
+              Sign up now and start your cinematic journey!
             </Text>
-          </Anchor>
-        </Text>
-        <Divider label="Or sign in with" labelPosition="center" my="lg" />
-        <Button
-          fullWidth
-          size="lg"
-          variant="default"
-          bg={"light-gray.1"}
-          fw={500}
-          leftSection={<Image src={GoogleLogo} h={20} alt="Google Logo" />}
-          onClick={signInWithGoogle}
-        >
-          Sign in with Google
-        </Button>
-      </Stack>
-    </Container>
+          </Container>
+          <Button component={Link} to="/auth/signup" size="lg" fullWidth>
+            Sign Up
+          </Button>
+          <Text ta="center" fz="sm">
+            I already have an account?
+            <Anchor component={Link} to="auth/login">
+              <Text component="span" weight={700} px="xs">
+                Login
+              </Text>
+            </Anchor>
+          </Text>
+          <Divider label="Or sign in with" labelPosition="center" my="lg" />
+          <Button
+            fullWidth
+            size="lg"
+            variant="default"
+            bg={"light-gray.1"}
+            fw={500}
+            leftSection={<Image src={GoogleLogo} h={20} alt="Google Logo" />}
+            onClick={signInWithGoogle}
+          >
+            Sign in with Google
+          </Button>
+        </Stack>
+      </Container>
+    </Flex>
   );
 }
