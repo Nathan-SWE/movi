119690e656f4d05ff18dbfd192b5bf2c8f06c855
Nathan Lima <nathanls.swe@gmail.com>
2025-10-15 13:52:29 -0300

    refactor: extract state and handlers from SignUpPage into custom hook

--- src/pages/Auth/SignUpPage.jsx
+++ src/pages/Auth/SignUpPage.jsx
@@ -1,8 +1,3 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { updateProfile } from "firebase/auth";
-import { z as zod } from "zod";
 import {
   Paper,
   Title,
@@ -15,93 +10,18 @@ import {
   Box,
   Stack,
   ActionIcon,
-} from "@mantine/core";
-import { useForm } from "@mantine/form";
-import { zod4Resolver } from "mantine-form-zod-resolver";
-import { IconChevronLeft } from "@tabler/icons-react";
+} from '@mantine/core';
+import { IconChevronLeft } from '@tabler/icons-react';
+import { Link } from 'react-router-dom';
 
-import { signUpWithEmail } from "../../features/authentication/authService";
 import {
   CustomTextInput,
   CustomPasswordInput,
-} from "../../components/ui/CustomInputs";
-
-const signUpSchema = zod
-  .object({
-    userName: zod
-      .string()
-      .min(3, { message: "Username must have at least 3 characters" })
-      .regex(/^[a-zA-Z0-9._-]+$/, {
-        message: "Username can only contain letters, numbers, and . - _",
-      })
-      .refine((name) => !/^[._-]/.test(name), {
-        message: "Username cannot start with a symbol",
-      }),
-    email: zod.email({ message: "Invalid Email" }),
-    password: zod
-      .string()
-      .min(8, { message: "Must have at least 8 characters " })
-      .regex(/[A-Z]/, { message: "Must contain at least one capital letter " })
-      .regex(/[a-z]/, {
-        message: "Must contain at least one lowercase letter ",
-      })
-      .regex(/[0-9]/, { message: "Must contain at least one number " })
-      .regex(/[^A-Za-z0-9]/, {
-        message: "Must contains at least one special character",
-      }),
-    confirmPassword: zod.string().min(8, {
-      message: "Password confirmation must have at least 8 characters",
-    }),
-    terms: zod.boolean().refine((val) => val === true, {
-      message: "You must accept the terms and conditions",
-    }),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "The passwords do not correspond",
-    path: ["confirmPassword"],
-  });
+} from '../../components/ui/CustomInputs';
+import useSingUpForm from '../../features/authentication/hooks/useSignUpForm';
 
 export default function SignUpPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const navigate = useNavigate();
-
-  const form = useForm({
-    initialValues: {
-      userName: "",
-      email: "",
-      password: "",
-      confirmPassword: "",
-      terms: false,
-    },
-    validate: zod4Resolver(signUpSchema),
-  });
-
-  const handleSubmit = async (values) => {
-    setIsLoading(true);
-    try {
-      const userCredential = await signUpWithEmail(
-        values.email,
-        values.password
-      );
-
-      if (userCredential) {
-        await updateProfile(userCredential, {
-          displayName: values.userName,
-        });
-      }
-
-      console.log("Account created successfully!");
-      navigate("/auth"); //temporário. redirecionar para home depois
-    } catch (error) {
-      if (error.code === "auth/email-already-in-use") {
-        form.setFieldError("email", "This email is already in use.");
-      } else {
-        console.log(`An unexpected error occurred: ${error.message}`);
-      }
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  const { form, isLoading, handleSubmit } = useSingUpForm();
 
   return (
     <Container size="xs" w="100%" mt="xl">
@@ -126,7 +46,7 @@ export default function SignUpPage() {
 
           <Stack gap="xxs">
             <Title align="center" order={2}>
-              Let's get started
+              Let&apos;s get started
             </Title>
             <Text align="center" c="dimmed" fz="sm">
               The latest movies and series are here
@@ -138,33 +58,33 @@ export default function SignUpPage() {
               <CustomTextInput
                 label="User name"
                 placeholder="user"
-                {...form.getInputProps("userName")}
+                {...form.getInputProps('userName')}
               />
               <CustomTextInput
                 label="Email Address"
                 placeholder="user@email.com"
-                {...form.getInputProps("email")}
+                {...form.getInputProps('email')}
               />
               <CustomPasswordInput
                 label="Password"
                 placeholder="password123"
-                {...form.getInputProps("password")}
+                {...form.getInputProps('password')}
               />
               <CustomPasswordInput
                 label="Confirm Password"
                 placeholder="password123"
-                {...form.getInputProps("confirmPassword")}
+                {...form.getInputProps('confirmPassword')}
               />
 
               <Checkbox
                 mt="md"
                 label={
                   <Text fz="sm">
-                    I agree to the <Anchor href="#">Terms and Services</Anchor>{" "}
+                    I agree to the <Anchor href="#">Terms and Services</Anchor>{' '}
                     and <Anchor href="#">Privacy Policy</Anchor>
                   </Text>
                 }
-                {...form.getInputProps("terms", { type: "checkbox" })}
+                {...form.getInputProps('terms', { type: 'checkbox' })}
               />
               <Button type="submit" fullWidth size="lg" loading={isLoading}>
                 Sign Up
